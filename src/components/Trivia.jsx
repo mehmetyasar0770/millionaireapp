07eff52100642dfce1c3d835103e0a54e9367e8d
mehmet_yasar <mehmet.yasar0770@gmail.com>
2023-12-21 16:54:51 +0300

    ilk düzenlemeler yapıldı

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Trivia() {
+  return (
+    <div className="trivia">
+        <div className="question">what's the best youtube channel?</div>
+        <div className="answers">Lama</div>
+        <div className="answers">camel</div>
+        <div className="answers">elephant</div>
+    </div>
+  )
+}
